da33079da1757b6f043ed54c1dd46742903095e4
Ryder He <r37he@edu.uwaterloo.ca>
2022-02-05 19:20:13 -0500

    Added CollectionItem & Styled CollectionPreview and CollectionItem

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,15 +1,18 @@
 import "./collection-preview.styles.scss";
+import CollectionItem from "../collection-item/collection-item.component";
 
 const CollectionPreview = ({ title, items }) => {
+    console.log("title: ", title)
+    console.log(items)
     return (
         <div className="collection-preview">
-            <h1>{title.toUpperCase()}</h1>
+            <h1 className="title">{title.toUpperCase()}</h1>
             <div className="preview">
                 {
                     items
                         .filter((item, idx) => idx < 4)
-                        .map(item => (
-                            <div key={item.id}>{item.name}</div>
+                        .map(({ id, ...otherItemProps }) => (
+                            <CollectionItem key={id} {...otherItemProps} />
                         ))
                 }
 
