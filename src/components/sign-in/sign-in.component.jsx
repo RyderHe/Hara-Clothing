42b33f476706e748de2587c677c3ad96752de023
Ryder He <r37he@edu.uwaterloo.ca>
2022-02-06 21:54:49 -0500

    Finished signin with password & email

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -2,16 +2,22 @@ import React, { useState } from 'react';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 import "./sign-in.styles.scss";
-import { signInWithGoogle } from "../../firebase/firebase.utils";
+import { auth, signInWithGoogle } from "../../firebase/firebase.utils";
 
 const SignIn = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
-    const handleSubmit = (event) => {
+    const handleSubmit = async (event) => {
         event.preventDefault();
-        setEmail()
-        setPassword();
+        try {
+            await auth.signInWithEmailAndPassword(email, password);
+            // If sign in with email & password succeed, clear the state
+            setEmail("")
+            setPassword("");
+        } catch(error) {
+            console.log(error);
+        }
     }
 
     const handleChange = (event) => {
